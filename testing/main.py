4c3a3e56abdfc6106401b3b125f47077f60203fa
Hozek-V <janvystup@seznam.cz>
2024-08-26 19:52:37 +0200

    desetinná čísla test

--- testing/main.py
+++ testing/main.py
@@ -14,11 +14,19 @@ class TestMathFunctions(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-2, 1), -1)
         self.assertEqual(add(-1, 7), 6)
+        self.assertEqual(add(1, -1), 0)
         self.assertEqual(add(3, -6), -3)
         self.assertEqual(add(10, -6), 4)
         self.assertEqual(add(-1, -3), -4)
 
+    def test_addition_floats(self):
+        self.assertEqual(add(1.2, 1.3), 2.5)
+        self.assertEqual(add(1.2, -1.3), -0.1)
+        self.assertEqual(add(-1.2, 1.3), 0.1)
+        self.assertEqual(add(-1.2, -1.3), -2.5)
+        # ..... 
 
     def test_multi(self):
         self.assertEqual(multiplication(2, 3), 6)

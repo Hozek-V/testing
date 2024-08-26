b76abbb9aa7569f7093f23bc56dff9be06573ba9
Hozek-V <janvystup@seznam.cz>
2024-08-26 20:07:54 +0200

    float numbert owerdue

--- testing/main.py
+++ testing/main.py
@@ -22,12 +22,15 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(add(-1, -3), -4)
 
     def test_addition_floats(self):
-        self.assertEqual(add(1.2, 1.3), 2.5)
-        self.assertEqual(add(1.2, -1.3), -0.1)
-        self.assertEqual(add(-1.2, 1.3), 0.1)
-        self.assertEqual(add(-1.2, -1.3), -2.5)
-        # ..... 
-
+        # AlmostEqual protože s desetinnými místy nepracuje přesně
+        self.assertAlmostEqual(add(1.2, 1.3), 2.5)
+        self.assertAlmostEqual(add(1.2, -1.3), -0.1)
+        self.assertAlmostEqual(add(-1.2, 1.3), 0.1)
+        self.assertAlmostEqual(add(-1.2, -1.3), -2.5)
+        # .....
+        # stanovím toleranci
+        result = add(1.2, 1.3)
+        self.assertTrue(result - 2.5 < 0.0001)
     def test_multi(self):
         self.assertEqual(multiplication(2, 3), 6)
         self.assertEqual(multiplication(-1, 1), -1)
